1af253b95a800ca6023f132fdfc50e1ab4fae822
weicheng xi <xiwc87@yeah.net>
2015-03-30 21:28:02 +0800

    repackage

---
+++ src/main/java/com/lhjz/portal/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+/**
+ * 立衡脊柱版权所有 (lhjz)
+ */
+package com.lhjz.portal.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lhjz.portal.domain.User;
+import com.lhjz.portal.domain.UserRepository;
+import com.lhjz.portal.service.UserService;
+
+/**
+ * 
+ * @author xi
+ * 
+ * @date 2015年3月28日 下午2:01:22
+ * 
+ */
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public User save(User user) {
+		return userRepository.save(user);
+	}
+}
---
+++ src/test/java/com/lhjz/portal/service/impl/UserServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.lhjz.portal.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+import com.lhjz.portal.domain.User;
+import com.lhjz.portal.service.UserService;
+
+public class UserServiceImplTest extends BaseServiceImplTest {
+
+	@Autowired
+	UserService userService;
+
+	@Test
+	public void save() {
+		userService.save(new User("name_xiwc", "password"));
+	}
+}
