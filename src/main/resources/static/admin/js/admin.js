6feeecec1ae90fcf0859156bec2f28161cb80f8c
weicheng xi <xiwc87@yeah.net>
2015-05-20 22:47:30 +0800

    文章删除确认实现

--- src/main/resources/static/admin/js/admin.js
+++ src/main/resources/static/admin/js/admin.js
@@ -1,64 +1,69 @@
 jQuery(function($) {
 
-	$('.ad-index-btn-menu').click(function() {
-		$('.ad-index-menu').sidebar('toggle');
-	});
+    $('.ad-index-btn-menu').click(function() {
+        $('.ad-index-menu').sidebar('toggle');
+    });
 
-	$('.ui.sticky').sticky({
-		offset : 84,
-		pushing : true,
-		bottomOffset: 84,
-		context: '.ad-index-container'
-	});
+    $('.ui.sticky').sticky({
+        offset: 84,
+        pushing: true
+        // bottomOffset: 84,
+        // context: '.ad-index-container'
+    });
 
-	$('.ad-index-logout').click(function() {
-		$(this).siblings('form').submit();
-	});
+    $('.ad-index-logout').click(function() {
+        $(this).siblings('form').submit();
+    });
 
-	// semantic-ui ajax api
-	$.fn.api.settings.api = {
-		'deleteFileById' : 'admin/file/delete?id={id}',
-		'updateFileName' : 'admin/file/update?id={id}&name={name}',
-		'saveArticle' : 'admin/article/save',
-		'updateArticle' : 'admin/article/update',
-		'deleteArticleById' : 'admin/article/delete?id={id}'
-	};
+    // semantic-ui ajax api
+    $.fn.api.settings.api = {
+        'deleteFileById': 'admin/file/delete?id={id}',
+        'updateFileName': 'admin/file/update?id={id}&name={name}',
+        'saveArticle': 'admin/article/save',
+        'updateArticle': 'admin/article/update',
+        'deleteArticleById': 'admin/article/delete?id={id}'
+    };
 
-	// toastr notification options
-	toastr.options = {
-		"closeButton" : true,
-		"debug" : false,
-		"newestOnTop" : true,
-		"progressBar" : false,
-		"positionClass" : "toast-bottom-center",
-		"preventDuplicates" : false,
-		"onclick" : null,
-		"showDuration" : "300",
-		"hideDuration" : "1000",
-		"timeOut" : "5000",
-		"extendedTimeOut" : "1000",
-		"showEasing" : "swing",
-		"hideEasing" : "linear",
-		"showMethod" : "fadeIn",
-		"hideMethod" : "fadeOut"
-	};
+    $.fn.api.settings.successTest = function(resp) {
+        if (resp && resp.success) {
+            return resp.success;
+        }
+        return false;
+    };
+
+    // toastr notification options
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": false,
+        "positionClass": "toast-bottom-center",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
 
 });
 
 jQuery(function($) {
-	// custom helper utils
-	window.Utils = window.Utils || {};
-	
-	$.extend(window.Utils, {
-		removeFileType: function(name){
-			var i = name.lastIndexOf('.');
-			return (i != -1) ? name.substring(0, i) : name;
-		},
-		getFileType: function(name){
-			var i = name.lastIndexOf('.');
-			return (i != -1) ? name.substring(i) : '';
-		}
-	});
+    // custom helper utils
+    window.Utils = window.Utils || {};
+
+    $.extend(window.Utils, {
+        removeFileType: function(name) {
+            var i = name.lastIndexOf('.');
+            return (i != -1) ? name.substring(0, i) : name;
+        },
+        getFileType: function(name) {
+            var i = name.lastIndexOf('.');
+            return (i != -1) ? name.substring(i) : '';
+        }
+    });
 });
-
-
